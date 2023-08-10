38e864ed1fd02b2d397b292d204dc3e62dcdf6a1
Shivam Swarnkar <swarnkarshivam974@gmail.com>
2023-08-10 13:40:19 +0530

    footer updated

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -22,12 +22,7 @@ const Footer = () => {
                     <li className="menuItem">FAQ</li>
                 </ul>
                 <div className="infoText">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur.
+                "Unlock the Magic of Cinema with [Your Movie Web App Name] - Your Ultimate Destination for Movie Enthusiasts. Explore, Discover, and Dive into a World of Entertainment. Lights, Camera, Action - Right at Your Fingertips!"
                 </div>
                 <div className="socialIcons">
                     <span className="icon">
